8eb53bcede59d2453390a449eeaf55da055987ce
Shane Lattanzio <pair+slattanzio@pivotal.io>
2017-08-09 15:14:47 -0400

    first commit

---
+++ components/blobstore/src/main/java/org/superbiz/moviefun/blobstore/UploadServlet.java
@@ -0,0 +1,7 @@
+package org.superbiz.moviefun.blobstore;
+
+/**
+ * Created by pivotal on 2017-08-09.
+ */
+public class UploadServlet {
+}
---
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/CoverCatalog.java
@@ -0,0 +1,7 @@
+package org.superbiz.moviefun.albumsapi;
+
+/**
+ * Created by pivotal on 2017-08-02.
+ */
+public class CoverCatalog {
+}
