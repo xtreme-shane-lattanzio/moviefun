cfd651ceea82de001cc27adb62eb7c1c7d8c1376
Shane Lattanzio <pair+slattanzio@pivotal.io>
2017-08-14 15:57:35 -0400

    Test

--- components/blobstore/src/main/java/org/superbiz/moviefun/blobstore/S3Store.java
+++ components/blobstore/src/main/java/org/superbiz/moviefun/blobstore/S3Store.java
@@ -49,19 +49,17 @@ public class S3Store implements BlobStore {
 //            return Optional.empty();
 //        }
 
-        logger.debug("OMGBUCKET" + name);
-        
         BlobId blobId1 = BlobId.of("moviefun", name);
         BlobId blobId2 = BlobId.of("moviefun", "4");
 
-        logger.debug("OMGBUCKET" + blobId1.getName());
-        logger.debug("OMGBUCKET" + blobId1.getBucket());
-        logger.debug("OMGBUCKET" + blobId2.getName());
-        logger.debug("OMGBUCKET" + blobId2.getBucket());
-        logger.debug("OMGBUCKET" + blobId1);
+        logger.debug("OMGBUCKETName1" + blobId1.getName());
+        logger.debug("OMGBUCKETBucket1" + blobId1.getBucket());
+        logger.debug("OMGBUCKETBlob1" + blobId1);
+        logger.debug("OMGBUCKETName2" + blobId2.getName());
+        logger.debug("OMGBUCKETBucket2" + blobId2.getBucket());
+        logger.debug("OMGBUCKETBlob2" + blobId2);
 
-
-        com.google.cloud.storage.Blob blob = s3.get(blobId1);
+        com.google.cloud.storage.Blob blob = s3.get(blobId2);
 
 //        S3Object s3Object = s3.getObject(bucketName, name);
 //        S3ObjectInputStream content = s3Object.getObjectContent();
